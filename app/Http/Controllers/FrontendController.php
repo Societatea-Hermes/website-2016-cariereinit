afe41cf02ab2091741698977c8135b98902596de
Cojocaru Cosmin <cosmin.cojocaru@societatea-hermes.ro>
2021-03-11 00:00:59 +0200

    order changed

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -22,7 +22,7 @@ class FrontendController extends Controller
     	$partnerLogos = User::select('packages.logo_size', 'users.id', 'users.full_name', 'users.site_url')
     						->join('packages', 'packages.id', '=', 'users.package_id')
     						->where('users.privilege', 2)
-                            ->orderByDesc("user.id")
+                            ->orderBy("user.id")
     						->get();
 
     	$logos = array();
